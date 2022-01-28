e2e371a920493b9a411aaf2927f23eb03f67527e
Prince Anuragi <prince@uniris.io>
2022-01-28 16:09:05 +0530

    Latest Commit

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ var figlet = require('figlet');
 //  } = require("./lib/handler");
 
  const { ArchethicHandler } = require('./lib/handler');
+ const archethic = require('archethic')
 
 // const { hideBin } = require('yargs/helpers')
 // const argv = yargs(hideBin(process.argv)).argv
@@ -19,15 +20,20 @@ var figlet = require('figlet');
 encrypted_key_plus_wallet = "0401EC530D1BBDF3B1B3E18C6E2330E5CFD1BFD88EB6D84102184CB39EC271793578B469ACBD8EB4F684C41B5DA87712A203AAA910B7964218794E3D3F343835843C44AFFE281D750E6CA526C6FC265167FE37DB9E47828BF80964DAC837E1072CA9954FF1852FF71865B9043BC117BC001C47D76A326A2A2F7CF6B16AB49E9E57F9D5E6D8E1D00D7F1B7E2F986C711DCA060005B2C8F485"
 address_index = "00000000"
 receiver = "020019CA33A6CA9E69B5C29E6E8497CC5AC9675952F847347709AD39C92C1B1B5313"
-amount = "1122334455667788"
+amount = "000000038407B700"
 
 // Address APDU
 const apdu_hex_payload_addr = address_index + encrypted_key_plus_wallet
 
 // Sign APDU
-const apdu_hex_payload = address_index + amount + receiver + encrypted_key_plus_wallet
+const apdu_hex_payload = address_index + receiver + amount + encrypted_key_plus_wallet
 
 const mainInstance = new ArchethicHandler();
+
+const txnBuilder = archethic.newTransactionBuilder("transfer");
+        txnBuilder.addUCOTransfer(receiver, 10.0)
+
+// console.log(txnBuilder.originSignaturePayload())
 
 yargs.command({
     command: 'about',
